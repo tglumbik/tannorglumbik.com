58925bbad51b6cdb7c4f5e4a52581a9c752036ab
tglumbik <60484108+tglumbik@users.noreply.github.com>
2021-03-15 11:44:51 -0700

    JS
    On Click and On Hover

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,21 @@
 console.log('Ok');
 
-// this works for only the first class item
-const trigger = document.querySelector('.portfolioItem');
-const target = document.querySelector('.portfolioItemText');
-const targetTwo = document.querySelector('.portfolioItemBG')
+const portfolio = document.querySelector('#portfolio');
 
-trigger.addEventListener('click', () => {
-    target.classList.toggle('portfolioOverlayText');
-    targetTwo.classList.toggle('portfolioOVerlayBG');
-})
+const smallDevice = window.matchMedia("(max-width: 700px)");
 
+// Sets Default
+portfolio.classList = 'portfolio';
+
+smallDevice.addListener(handleDeviceChange);
+
+function handleDeviceChange(e) {
+    if (e.matches) portfolio.addEventListener('click', () => {
+        portfolio.classList.toggle('portfolioOverlay');
+    });
+    else portfolio.classList = 'portfolio';
+}
+
+// Run it initially
+handleDeviceChange(smallDevice);
 
